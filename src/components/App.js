f8c294793a59111e63403e72ea6fcce9a43cce1f
adharamonzon <adharamonzon@gmail.com>
2020-04-01 17:21:31 +0200

    add all colapsable sections

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../stylesheets/main.scss';
 import Header from './header';
+import Collapse from './collapse';
 import Footer from './footer';
 
 function App() {
   return (
     <div /* className="App" */>
-      <Header/>
-      <Footer/>
+      <Header />
+      <Collapse title='Diseña' />
+      <Collapse title='Rellena' />
+      <Collapse title='Comparte' icon='fas fa-share-alt' />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/collapse.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Collapse = props => {
+  return (
+    <section>
+      <div className='share js-share-title'>
+        <span className='share--icon1'>
+          <i className={props.icon}></i>
+        </span>
+        <h3 className='share--text'>{props.title}</h3>
+        <a className='share--icon2'>
+          <i className='js-arrow fas fa-angle-up'></i>
+        </a>
+      </div>
+      <div>{props.children}</div>
+    </section>
+  );
+};
+
+export default Collapse;
