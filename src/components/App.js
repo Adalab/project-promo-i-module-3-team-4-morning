84ee96c7ac270d1cab8089075949e11799365588
adharamonzon <adharamonzon@gmail.com>
2020-04-01 11:40:20 +0200

    add JSX footer, add css styles #10

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../stylesheets/main.scss';
 import Header from './header';
+import Footer from './footer';
 
 function App() {
   return (
     <div /* className="App" */>
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import AdalabLogo from '../images/logo-adalab.png';
+
+const Footer = () => {
+  return(
+  <footer>
+  <div className="footer">
+    <div className="footer--title">
+      <small>
+        Awesome profile-cards @2018
+      </small>
+    </div>
+    <div>
+      <img className="footer--logo" src={AdalabLogo} alt="Logo Adalab" />
+    </div>
+  </div>
+</footer>
+);
+}
+
+export default Footer; 
