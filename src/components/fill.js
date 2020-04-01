b4e30b18edf546acfc0bb0dd0d9a3ed33f78371a
violetagonza <violetagonzalezcalleja@gmail.com>
2020-04-01 20:41:00 +0200

    Change inputs with components of inputs #15

--- src/components/fill.js
+++ src/components/fill.js
@@ -11,18 +11,20 @@ const Fill = props => {
             Nombre completo <span className='form-object'> *</span>
           </label>
           <Input id='form--name' type='text' name='form--name' placeholder='Ej: Sally Jill' />
-          <input className='js-fill-name' id='form--name' type='text' name='form--name' placeholder='Ej: Sally Jill' required />
+
           {/* <!-- Puesto --> */}
           <label htmlFor='form--job'>
             Puesto <span className='form-object'> *</span>
           </label>
-          <input className='js-fill-job' id='form--job' type='text' name='form--job' placeholder='Ej: Front-end unicorn' required />
+          <Input id='form--job' type='text' name='form--job' placeholder='Ej: Front-end unicorn' />
+
           {/* <!-- Boton para subir imagen --> */}
           <label htmlFor='form--img'>
             Imagen de perfil<span className='form-object'> *</span>
           </label>
           <div className='form--file'>
-            <input className='form--file__input js-fill-file' type='file' accept='image/png, .jpeg, .jpg, image/gif' />
+            <Input className='form--file__input' type='file' name='form--job' />
+            {/* <input className='form--file__input js-fill-file' type='file' accept='image/png, .jpeg, .jpg, image/gif' /> */}
           </div>
           <div className='form--button__container'>
             <button className='form--button'>Añadir imagen</button>
@@ -33,21 +35,24 @@ const Fill = props => {
           <label htmlFor='form--email'>
             Email <span className='form-object'> *</span>
           </label>
-          <input className='js-fill-email' type='email' id='form--email' name='form--email' placeholder='Ej: sally-hill@gmail.com' required />
+          <Input id='form--email' type='email' name='form--email' placeholder='Ej: sally-hill@gmail.com' />
+
           {/* <!-- Telefono --> */}
           <label htmlFor='form--phone'>
             Teléfono <span className='form-object'> *</span>
           </label>
-          <input className='js-fill-tel' type='tel' id='form--phone' name='form--phone' placeholder='555-55-55-55' required />
+          <Input id='form--phone' type='tel' name='form--phone' placeholder='555-55-55-55' />
+
           {/* <!-- Linkedin --> */}
           <label htmlFor='form--linkedin'>
             Linkedin <span className='form-object'> *</span>
           </label>
-          <input className='js-fill-linkedin' id='form--linkedin' type='text' name='form--linkedin' placeholder='Ej: linkedin.com/in/sally.hill' required />
+          <Input id='form--linkedin' type='text' name='form--linkedin' placeholder='Ej: linkedin.com/in/sally.hill' />
+
           <label htmlFor='form--job'>
             Github<span className='form-object'> *</span>
           </label>
-          <input className='js-fill-github' id='form--github' type='text' name='form--github' placeholder='Ej: @sally-hill' required />
+          <Input id='form--github' type='text' name='form--github' placeholder='Ej: github.com/sally-hill' />
         </form>
       </div>
     </section>
