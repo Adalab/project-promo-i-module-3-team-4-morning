4d23ac32f32602eb2fa103982a37c04207bf9ac3
violetagonza <violetagonzalezcalleja@gmail.com>
2020-04-06 20:52:28 +0200

    Update docs

---
+++ docs/precache-manifest.993bceec1a6ec8029bde8ac8d6b84b1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f077d28720b25df60948d945a8c71b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b64c950673fb8034256",
+    "url": "./static/css/main.a5b8066f.chunk.css"
+  },
+  {
+    "revision": "c87c22a0bd8edde0dad9",
+    "url": "./static/js/2.aa36c27c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.aa36c27c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b64c950673fb8034256",
+    "url": "./static/js/main.238b899f.chunk.js"
+  },
+  {
+    "revision": "2816ffeafe738c28a6b4",
+    "url": "./static/js/runtime-main.29a49edd.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logo-awesome-profile-cards.0118e311.svg"
+  },
+  {
+    "revision": "010da22f858bc99cf08ae015319b7aa6",
+    "url": "./static/media/preview-photo.010da22f.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00a11ff04841360b9d8f86558d7e801d.js"
+  "./precache-manifest.993bceec1a6ec8029bde8ac8d6b84b1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
