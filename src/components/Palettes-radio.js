88f9bab4f58b256553c83522196457a4df9ee5a1
adharamonzon <adharamonzon@gmail.com>
2020-04-06 13:29:35 +0200

    add label to radio input

--- src/components/Palettes-radio.js
+++ src/components/Palettes-radio.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ColorsRadio from "./Palettes-color-radio";
+import React from 'react';
+import ColorsRadio from './Palettes-color-radio';
 
 const Radio = (props) => {
   return (
-    <div className="design--form__item">
-      <input id={props.id} className={props.className} type="radio" name="select-palette" />
+    <label className='design--form__item' htmlFor={props.id}>
+      <input id={props.id} className={props.className} type='radio' name='select-palette' />
       <ColorsRadio paletteClass={props.paletteClass} colorA={props.colorA} colorB={props.colorB} colorC={props.colorC} />
-    </div>
+    </label>
   );
 };
 
