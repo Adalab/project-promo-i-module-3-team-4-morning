ab8f2ad6040af633e045df53d6f0f390e19e00fa
violetagonza <violetagonzalezcalleja@gmail.com>
2020-04-10 12:47:25 +0200

    :construction: Add state and event to change icon to closed #21

--- src/components/App.js
+++ src/components/App.js
@@ -10,22 +10,33 @@ import Share from './Share';
 
 function App() {
   const [palette, setPalette] = useState('1');
+  const [iconStyle, setIconStyle] = useState('');
   const handlePalette1 = (id) => {
     setPalette(id);
   };
-  console.log('state:' + palette);
+  const handleCollapse1 = (id) => {
+    setIconStyle('closed');
+  };
+  const handleCollapse2 = (id) => {
+    setIconStyle('closed');
+  };
+  const handleCollapse3 = (id) => {
+    setIconStyle('closed');
+  };
+  console.log('state:' + iconStyle);
   return (
     <div /* className="App" */>
       <Header />
       <main className='main'>
         <Card state={palette} />
         <section className='information'>
-          <Collapse title='Diseña' icon='far fa-object-ungroup' />
-          <Palettes handleChange={handlePalette1} />
-          <Collapse title='Rellena' icon='far fa-keyboard'>
+          <Collapse close={iconStyle} id='collapse-1' title='Diseña' icon='far fa-object-ungroup' handleCollapse={handleCollapse1}>
+            <Palettes handleChange={handlePalette1} />
+          </Collapse>
+          <Collapse close={iconStyle} id='collapse-2' title='Rellena' icon='far fa-keyboard' handleCollapse={handleCollapse2}>
             <Fill></Fill>
           </Collapse>
-          <Collapse title='Comparte' icon='fas fa-share-alt'>
+          <Collapse close={iconStyle} id='collapse-3' title='Comparte' icon='fas fa-share-alt' handleCollapse={handleCollapse3}>
             <Share />
           </Collapse>
         </section>
