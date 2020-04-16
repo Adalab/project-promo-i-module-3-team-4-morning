1a25851099586b2c2ab0b39017097f0618699855
violetagonza <violetagonzalezcalleja@gmail.com>
2020-04-16 21:25:48 +0200

    :recycle: Check divs and sections so it looks like the original project and fix margins #21

--- src/components/collapse.js
+++ src/components/collapse.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Collapse = (props) => {
   function handleClick(ev) {
@@ -7,19 +7,19 @@ const Collapse = (props) => {
 
     //aquí tengo que encontrar una manera de que cuando clicke de true o false, eso lo paso por handleCollapse y es lo que le meto al estado en App
   }
-
+  // "share js-share-title"
   return (
-    <section>
-      <div id={props.id} onClick={handleClick} className="share js-share-title">
-        <span className="share--icon1">
+    <section className={props.margin}>
+      <div id={props.id} onClick={handleClick} className={props.colClass}>
+        <span className='share--icon1'>
           <i className={props.icon}></i>
         </span>
-        <h3 className="share--text">{props.title}</h3>
-        <a href="/" className="share--icon2">
+        <h3 className='share--text'>{props.title}</h3>
+        <a href='/' className='share--icon2'>
           <i className={`js-arrow fas fa-angle-up ${props.close}`}></i>
         </a>
       </div>
-      <div>{props.children}</div>
+      {props.children}
     </section>
   );
 };
