1878a88975d99483731eeb46a8501b9345f225d9
violetagonza <violetagonzalezcalleja@gmail.com>
2020-04-21 19:19:49 +0200

    Update docs

---
+++ docs/precache-manifest.003aafa7186831b7625b105c72ea4a28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0ed0c070b08e3c1d0864d226599b5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8bd71653fb9b09c80bb",
+    "url": "./static/css/main.238c98e3.chunk.css"
+  },
+  {
+    "revision": "4cb0a7d2a9c9cf5e3692",
+    "url": "./static/js/2.ab8551a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ab8551a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8bd71653fb9b09c80bb",
+    "url": "./static/js/main.970c428a.chunk.js"
+  },
+  {
+    "revision": "2816ffeafe738c28a6b4",
+    "url": "./static/js/runtime-main.29a49edd.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logo-awesome-profile-cards.0118e311.svg"
+  },
+  {
+    "revision": "010da22f858bc99cf08ae015319b7aa6",
+    "url": "./static/media/preview-photo.010da22f.jpg"
+  }
+]);
